b0f7f999177e07ba0df714d12cdc855965446a0f
unknown <hur.abbas@kryptomind.com>
2021-07-17 15:43:06 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
-import cors from 'cors';
+// import cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config: ConfigService = app.get(ConfigService);
 
-  const options = {
-    origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.use(cors(options));
+  // const options = {
+  //   origin: ['http://localhost:3000'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true,
+  // };
+  // app.use(cors(options));
 
   // global validation pipe
   app.useGlobalPipes(new ValidationPipe());
